c7c41af11aeae6c1c7f0187203bc45581b2d0afd
Aneeb1231 <221322@students.au.edu.pk>
2025-06-17 14:22:40 +0500

    Update useProfile.js

--- src/hooks/useProfile.js
+++ src/hooks/useProfile.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:1000/api';
+const API_URL = 'https://easeaixbackend.onrender.com/api';
 
 const useProfile = () => {
   const [profile, setProfile] = useState(null);
